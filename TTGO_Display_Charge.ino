45606feed4b0f77fa26634df437e1d547b4f6625
joehou45 <hou.joe@gmail.com>
2021-08-19 00:27:21 +0800

    for charge test
    for charge test

---
+++ TTGO_Display_Charge.ino
@@ -0,0 +1,291 @@
+/*
+  Example animated analogue meters using a ILI9341 TFT LCD screen
+  Needs Font 2 (also Font 4 if using large scale label)
+  Make sure all the display driver and pin comnenctions are correct by
+  editting the User_Setup.h file in the TFT_eSPI library folder.
+  #########################################################################
+  ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
+  #########################################################################
+*/
+
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#include "esp_adc_cal.h"
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+#define TFT_GREY 0x5AEB
+
+#define LOOP_PERIOD 35 // Display updates every 35 ms
+#define ADC_EN              14  //ADC_EN is the ADC detection enable port
+#define ADC_PIN             34
+
+float ltx = 0;    // Saved x coord of bottom of needle
+uint16_t osx = 120, osy = 120; // Saved x & y coords
+uint32_t updateTime = 0;       // time for next update
+int vref = 1100;
+float battery_voltage = 0;
+
+int old_analog =  -999; // Value last displayed
+int old_digital = -999; // Value last displayed
+
+int value[6] = {0, 0, 0, 0, 0, 0};
+int old_value[6] = { -1, -1, -1, -1, -1, -1};
+int d = 0;
+bool Incharge = 0;
+
+void setup(void) {
+  tft.init();
+  tft.setRotation(1);
+  Serial.begin(115200); // For debug
+  tft.fillScreen(TFT_BLACK);
+
+  analogMeter(); // Draw analogue meter
+
+  esp_adc_cal_characteristics_t adc_chars;
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);    //Check type of calibration value used to characterize ADC
+  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+    Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
+    vref = adc_chars.vref;
+  } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+    Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
+  } else {
+    Serial.println("Default Vref: 1100mV");
+  }
+  updateTime = millis(); // Next update time
+}
+
+void loop() {
+  if (updateTime <= millis()) {
+    updateTime = millis() + LOOP_PERIOD;
+
+    plotPointer();
+    plotNeedle(value[0], 0);
+  }
+}
+
+
+// #########################################################################
+//  Draw the analogue meter on the screen
+// #########################################################################
+void analogMeter()
+{
+  // Meter outline
+  tft.fillRect(0, 0, 239, 126, TFT_GREY);
+  tft.fillRect(5, 3, 230, 119, TFT_WHITE);
+  tft.setTextColor(TFT_BLACK);  // Text colour
+
+  // Draw ticks every 5 degrees from -50 to +50 degrees (100 deg. FSD swing)
+  for (int i = -50; i < 51; i += 5) {
+    // Long scale tick length
+    int tl = 15;
+
+    // Coodinates of tick to draw
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (100 + tl) + 120;
+    uint16_t y0 = sy * (100 + tl) + 140;
+    uint16_t x1 = sx * 100 + 120;
+    uint16_t y1 = sy * 100 + 140;
+
+    // Coordinates of next tick for zone fill
+    float sx2 = cos((i + 5 - 90) * 0.0174532925);
+    float sy2 = sin((i + 5 - 90) * 0.0174532925);
+    int x2 = sx2 * (100 + tl) + 120;
+    int y2 = sy2 * (100 + tl) + 140;
+    int x3 = sx2 * 100 + 120;
+    int y3 = sy2 * 100 + 140;
+
+    //  Eed zone limits
+    if (i >= -50 && i < -25) {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_RED);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, TFT_RED);
+    }
+
+   //  Orange zone limits
+    if (i >= -25 && i < 0) {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_ORANGE);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, TFT_ORANGE);
+    }
+
+    // Yellow zone limits
+    if (i >= 0 && i < 25) {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_YELLOW);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, TFT_YELLOW);
+    }
+
+    // Green zone limits
+    if (i >= 25 && i < 50) {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_GREEN);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, TFT_GREEN);
+    }
+
+    // Short scale tick length
+    if (i % 25 != 0) tl = 8;
+
+    // Recalculate coords incase tick lenght changed
+    x0 = sx * (100 + tl) + 120;
+    y0 = sy * (100 + tl) + 140;
+    x1 = sx * 100 + 120;
+    y1 = sy * 100 + 140;
+
+    // Draw tick
+    tft.drawLine(x0, y0, x1, y1, TFT_BLACK);
+
+    // Check if labels should be drawn, with position tweaks
+    if (i % 25 == 0) {
+      // Calculate label positions
+      x0 = sx * (100 + tl + 10) + 120;
+      y0 = sy * (100 + tl + 10) + 140;
+      switch (i / 25) {
+        case -2: tft.drawCentreString("0", x0, y0 - 12, 2); break;
+        case -1: tft.drawCentreString("25", x0, y0 - 9, 2); break;
+        case 0: tft.drawCentreString("50", x0, y0 - 6, 2); break;
+        case 1: tft.drawCentreString("75", x0, y0 - 9, 2); break;
+        case 2: tft.drawCentreString("100", x0, y0 - 12, 2); break;
+      }
+    }
+
+    // Now draw the arc of the scale
+    sx = cos((i + 5 - 90) * 0.0174532925);
+    sy = sin((i + 5 - 90) * 0.0174532925);
+    x0 = sx * 100 + 120;
+    y0 = sy * 100 + 140;
+    // Draw scale arc, don't draw the last part
+    if (i < 50) tft.drawLine(x0, y0, x1, y1, TFT_BLACK);
+  }
+
+  tft.drawCentreString("%BATT", 120, 70, 4); // Comment out to avoid font 4
+  tft.drawRect(5, 3, 230, 119, TFT_BLACK); // Draw bezel line
+
+  plotNeedle(0, 0); // Put meter needle at 0
+}
+
+// #########################################################################
+// Update needle position
+// This function is blocking while needle moves, time depends on ms_delay
+// 10ms minimises needle flicker if text is drawn within needle sweep area
+// Smaller values OK if text not in sweep area, zero for instant movement but
+// does not look realistic... (note: 100 increments for full scale deflection)
+// #########################################################################
+void plotNeedle(int value, byte ms_delay)
+{
+  tft.setTextColor(TFT_RED, TFT_WHITE);
+//  char buf[8]; dtostrf(value, 4, 0, buf);
+
+  char buf[8]; dtostrf(battery_voltage, 4, 2, buf);
+  tft.drawRightString(buf, 40, 119 - 20, 2);
+  tft.drawString("V",45,119 - 20,2);
+
+  char percentage[8]; dtostrf(value, 4, 0, percentage);
+  tft.drawString(percentage, 190, 119 - 20, 2);
+  tft.drawString("%",220,119 - 20,2);
+  
+ Serial.println(battery_voltage);
+ Serial.println(buf);
+  if (value < -10) value = -10; // Limit value to emulate needle end stops
+  if (value > 110) value = 110;
+
+  // Move the needle util new value reached
+  while (!(value == old_analog)) {
+    if (old_analog < value) old_analog++;
+    else old_analog--;
+
+    if (ms_delay == 0) old_analog = value; // Update immediately id delay is 0
+
+    float sdeg = map(old_analog, -10, 110, -150, -30); // Map value to angle
+    // Calcualte tip of needle coords
+    float sx = cos(sdeg * 0.0174532925);
+    float sy = sin(sdeg * 0.0174532925);
+
+    // Calculate x delta of needle start (does not start at pivot point)
+    float tx = tan((sdeg + 90) * 0.0174532925);
+
+    // Erase old needle image
+    tft.drawLine(120 + 20 * ltx - 1, 140 - 20, osx - 1, osy, TFT_WHITE);
+    tft.drawLine(120 + 20 * ltx, 140 - 20, osx, osy, TFT_WHITE);
+    tft.drawLine(120 + 20 * ltx + 1, 140 - 20, osx + 1, osy, TFT_WHITE);
+
+    // Re-plot text under needle
+    tft.setTextColor(TFT_BLACK);
+
+    // Store new needle end coords for next erase
+    ltx = tx;
+    osx = sx * 98 + 120;
+    osy = sy * 98 + 140;
+
+    // Draw the needle in the new postion, magenta makes needle a bit bolder
+    // draws 3 lines to thicken needle
+    tft.drawLine(120 + 20 * ltx - 1, 140 - 20, osx - 1, osy, TFT_RED);
+    tft.drawLine(120 + 20 * ltx, 140 - 20, osx, osy, TFT_MAGENTA);
+    tft.drawLine(120 + 20 * ltx + 1, 140 - 20, osx + 1, osy, TFT_RED);
+
+    // Slow needle down slightly as it approaches new postion
+    if (abs(old_analog - value) < 10) ms_delay += ms_delay / 5;
+
+    // Wait before next update
+    delay(ms_delay);
+  }
+}
+
+// #########################################################################
+//  Adjust 6 linear meter pointer positions
+// #########################################################################
+void plotPointer(void)
+{
+  int dy = 187;
+  byte pw = 16;
+  uint16_t v = analogRead(ADC_PIN);
+  static uint64_t timeStamp = 0;
+  
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+
+  // Move the 6 pointers one pixel towards new value
+  for (int i = 0; i < 1; i++)
+  {
+    char buf[8]; dtostrf(value[i], 4, 0, buf);
+  //  tft.drawRightString(buf, i * 40 + 36 - 5, 187 - 27 + 155 - 18, 2);
+
+    int dx = 3 + 40 * i;
+    if (value[i] < 0) value[i] = 0; // Limit value to emulate needle end stops
+    if (value[i] > 100) value[i] = 100;
+
+    while (!(value[i] == old_value[i])) {
+      dy = 187 + 100 - old_value[i];
+      if (old_value[i] > value[i])
+      {
+        tft.drawLine(dx, dy - 5, dx + pw, dy, TFT_WHITE);
+        old_value[i]--;
+        tft.drawLine(dx, dy + 6, dx + pw, dy + 1, TFT_RED);
+      }
+      else
+      {
+        tft.drawLine(dx, dy + 5, dx + pw, dy, TFT_WHITE);
+        old_value[i]++;
+        tft.drawLine(dx, dy - 6, dx + pw, dy - 1, TFT_RED);
+      }
+    }
+
+    if (millis() - timeStamp > 1000) {
+      timeStamp = millis();
+      battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+      value[i] = (battery_voltage - 3.2)/((4.3-3.2)/100);
+      if (value[i] >= 100) 
+        value[i] = 100;
+        
+     // Serial.println(value[i]);
+     // Serial.println(battery_voltage);
+      
+      if ((battery_voltage > 4.85) && !Incharge) {
+        Incharge = 1; 
+        tft.fillRect(70, 55, 105, 65, TFT_WHITE);
+        tft.setTextColor(TFT_BLUE);  // Text colour
+        tft.drawCentreString("CHARG", 120, 70, 4); // Comment out to avoid font 4
+      } else if ((battery_voltage < 4.85) && Incharge){
+        Incharge = 0;
+        tft.fillRect(70, 55, 105, 65, TFT_WHITE);
+        tft.setTextColor(TFT_BLACK);  // Text colour
+        tft.drawCentreString("%BATT", 120, 70, 4); // Comment out to avoid font 4
+      }
+    }
+  }
+}
